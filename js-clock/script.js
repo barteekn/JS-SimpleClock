bba8feaaf5233dd3a391a3125db6b73000294fc7
Bartek Nowakowski <nowakowskib@gmail.com>
2022-06-27 17:55:45 +0200

    Small updates

--- js-clock/script.js
+++ js-clock/script.js
@@ -1,9 +1,17 @@
-function display() {
-    let d = new Date();
-    let h = d.getHours();
-    let m = d.getMinutes();
-    let s = d.getSeconds();
-    let time = h + ":" + m + ":" + s;
-    document.getElementById("time").innerHTML = time;
+function displayClock() {
+  let d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+  let time = h + ":" + m + ":" + s;
+  document.getElementById("time").innerHTML = time;
 }
-setInterval(display, 1000);
+setInterval(displayClock, 100);
+
+const date = new Date();
+const day = date.getDay();
+const month = date.getMonth();
+const year = date.getFullYear();
+const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("date").innerHTML = dayName[day] + ", " + monthName[month] + " " + year;
